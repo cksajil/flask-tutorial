2d7c91b9c97308c55a0efa4d496db92ef1859397
cksajil <sajilck@gmail.com>
2025-06-17 19:23:19 +0530

    Version 6 with css

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-student = ["Anna", "Sajil"]
-
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html", person=student)
+    return render_template("home.html")
