48bb508591d0e5056a591e7dee94faae09352f8a
cksajil <sajilck@gmail.com>
2025-06-17 19:32:06 +0530

    Version 7 added interactivity

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html")
+
+
+@app.route("/result", methods=["POST"])
+def result_page():
+    user_name = request.form["user_name"]
+    return render_template("result.html", name=user_name)
