c8e4e6319b922bbc2a7620cf8a5ba1d33ff10ae3
cksajil <sajilck@gmail.com>
2025-06-17 18:54:18 +0530

    Version 2 multiple pages

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return '<h1>Welcome to DSA Flask Tutorial</h1><br>See batch details <a href="http://127.0.0.1:5000/about">here'
+
+
+@app.route("/about")
+def hello():
+    return "<h1>From DSA SMP Feb Online Batch</h1>"
