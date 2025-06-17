33684b77953164885a3a918590736c675539ab58
cksajil <sajilck@gmail.com>
2025-06-17 19:01:15 +0530

    Version 3 rendering templates

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return '<h1>Welcome to DSA Flask Tutorial</h1><br>See batch details <a href="http://127.0.0.1:5000/about">here'
-
-
-@app.route("/about")
-def hello():
-    return "<h1>From DSA SMP Feb Online Batch</h1>"
+    return render_template("home.html")
